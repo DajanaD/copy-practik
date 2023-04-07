47f55ab1f6c9dcb0fb10df257a46cd99c223840c
Diana <ccv080480@gmail.com>
2023-04-07 16:52:03 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.corusel_innet').slick(
+        {
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true
+          }
+    );
+  });
